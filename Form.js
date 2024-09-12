0335b20508751389af3d6a164039041cb7d115ca
nandani2003 <nandaniverma2701@gmail.com>
2024-09-12 19:59:06 +0530

    form

---
+++ Form.js
@@ -0,0 +1,22 @@
+let  submit=document.getElementById("submit");
+submit.addEventListener("click",
+  displaydata);
+let row=1;
+function displaydata(e){
+  e.preventDefault();
+  let formData = [
+    document.getElementById("name").value,
+    document.getElementById("email").value,
+    document.getElementById("contact").value,
+    document.getElementById("age").value
+  ];
+
+  let newRow = display.insertRow(row);
+  formData.forEach((data, index) => {
+    newRow.insertCell(index).innerHTML = data;
+  });
+
+  display.style.display = 'block';
+
+}
+
